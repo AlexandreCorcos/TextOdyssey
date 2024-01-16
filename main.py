00b52dbed8bf05bbbc8965f698cc14dbfcfcac3e
Alexandre Corcos <acorcos@gmail.com>
2024-01-16 14:02:08 +0000

    started main.py file and some notes

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 """
 TextOdyssey Start
+
+# AI Model -> Retrieval Augmented Generation (RAG)
+
+# Memory for LLM
+To know all of the previous past, all of the previous replies, so it can generate an accurate
+reply going forward for our choose your own adventure game
+
+# Vector Database provided by Datastax
 """
